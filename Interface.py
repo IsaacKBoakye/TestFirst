0aba88de76471af77e16c45ef958afdea98eb0ac
Aaron Eisses <2683848+aeisses@users.noreply.github.com>
2021-10-04 20:43:13 -0300

    Made a test app with columns

--- Interface.py
+++ Interface.py
@@ -5,11 +5,37 @@
 # 1-Import
 import PySimpleGUI as sg
 
+box_list_column = [
+    [
+        sg.Text("Hello"),
+        ],[
+        sg.Text("To")
+        ],[
+        sg.Text("You")
+    ]
+]
+
+other_list_column = [
+    [
+        sg.Text("Ghana"),
+        ],[
+            sg.Text("Africa"),
+            ],[
+                sg.Text("Nova Scotia"),
+            ]
+        ]
 # 2- Layout
 
-layout = [[sg.Text('Enter something: '), sg.Input(key='-IN-')],
-         [sg.Text('Our output will go here', size=(30,1),key='-OUT-')],
-         [sg.Button('OK'), sg.Button('Exit')]]
+layout = [
+        [
+            sg.Column(box_list_column),
+            sg.VSeperator(),
+            sg.Column(other_list_column),
+            ]
+        ]
+#layout = [[sg.Text('Enter something: '), sg.Input(key='-IN-')],
+#         [sg.Text('Our output will go here', size=(30,1),key='-OUT-')],
+#         [sg.Button('OK'), sg.Button('Exit')]]
 
 # 3- Window
 window = sg.Window('Dash Board', layout)
